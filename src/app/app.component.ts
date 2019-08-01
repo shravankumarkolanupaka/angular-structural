ab148506e60b900e9f7af999a17c3232aef52ba8
shravankumarkolanupaka <shravankumar.lgit@gmail.com>
2019-08-01 19:59:47 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,25 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Angular';
   status1 = false;
+  
+  randomcolor = this.getRandomColor();
+
+    //function to get random colors
+    public getRandomColor() {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++){
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    //function to set a new random color
+    setColor() {
+        this.randomcolor = this.getRandomColor();
+    }
+
+
   technlogies = [{
    name:"HTML",
    desc:"Hypertext Markup Language is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript",
